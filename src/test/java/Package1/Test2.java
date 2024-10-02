c834919c6849dbae637f300bfb9b9340c927e5d7
avi210992 <avisimma003@gmail.com>
2024-10-02 12:30:34 +0530

    User2 second commit

--- src/test/java/Package1/Test2.java
+++ src/test/java/Package1/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		System.out.println("User2 first entry");
-		System.out.println("User1 first entry");
+		System.out.println("User1 first entry- updated by user2");
 		
 	}
 
